c4c9d0644871584d40266bc3976189fee7c7c7f3
Clément Nardi <clementnardi@gmail.com>
2016-10-01 01:16:43 +0200

    new object model

--- Hypersonic/hypersonic.go
+++ Hypersonic/hypersonic.go
@@ -25,6 +25,10 @@ const (
 	empty                = '.'
 	nbCardinalDirections = 4
 	bombTimer            = 8
+	itemNone             = 0
+	itemExtraRange       = 1
+	itemExtraBomb        = 2
+	maxPlayers           = 4
 )
 
 var cardinalVectors [nbCardinalDirections]Position = [nbCardinalDirections]Position{Position{0, 1}, Position{0, -1}, Position{1, 0}, Position{-1, 0}}
@@ -59,24 +63,212 @@ func Add(a, b Position) Position {
 }
 
 type Player struct {
-	Position
 	remainingBombs int
 	bombRange      int
+	isDead         bool
 }
 
 type Bomb struct {
 	Position
-	ownerID             int
-	nbRoundsToExplosion int
-	explosionRange      int
+	ownerID int
 }
 
-type Item struct {
-	Position
-	itemType int
+type Cell int16
+
+// prevent player from walking:
+// bit : bomb from player 0
+// bit : bomb from player 1
+// bit : bomb from player 2
+// bit : bomb from player 3
+// bit : wall
+// bit : box without item
+// bit : box with item type 1 extra range
+// bit : box with item type 2 extra bomb
+//
+// don't prevent players from walking:
+// bit : player 0
+// bit : player 1
+// bit : player 2
+// bit : player 3
+// bit : item type 1 extra range
+// bit : item type 2 extra bomb
+const (
+	bombPlayer0Bit    = 01
+	bombPlayer1Bit    = 02
+	bombPlayer2Bit    = 04
+	bombPlayer3Bit    = 010
+	wallBit           = 020
+	boxWithoutItemBit = 040
+	boxWithItem1Bit   = 0100
+	boxWithItem2Bit   = 0200
+	player0Bit        = 0400
+	player1Bit        = 01000
+	player2Bit        = 02000
+	player3Bit        = 04000
+	item1Bit          = 010000
+	item2Bit          = 020000
+
+	inaccessibleBits = 0377
+	boxBits          = 0340
+	bombBits         = 017
+	playerBits       = 07400
+	itemBits         = 030000
+)
+
+func (c *Cell) SetBomb(playerId int) {
+	switch playerId {
+	case 0:
+		*c |= bombPlayer0Bit
+	case 1:
+		*c |= bombPlayer1Bit
+	case 2:
+		*c |= bombPlayer2Bit
+	case 3:
+		*c |= bombPlayer3Bit
+	}
+}
+func (c *Cell) ResetBomb(playerId int) {
+	switch playerId {
+	case 0:
+		*c &^= bombPlayer0Bit
+	case 1:
+		*c &^= bombPlayer1Bit
+	case 2:
+		*c &^= bombPlayer2Bit
+	case 3:
+		*c &^= bombPlayer3Bit
+	}
+}
+func (c *Cell) SetPlayer(playerId int) {
+	switch playerId {
+	case 0:
+		*c |= player0Bit
+	case 1:
+		*c |= player1Bit
+	case 2:
+		*c |= player2Bit
+	case 3:
+		*c |= player3Bit
+	}
+}
+func (c *Cell) ResetPlayer(playerId int) {
+	switch playerId {
+	case 0:
+		*c &^= player0Bit
+	case 1:
+		*c &^= player1Bit
+	case 2:
+		*c &^= player2Bit
+	case 3:
+		*c &^= player3Bit
+	}
+}
+func (c *Cell) SetWall() {
+	*c |= wallBit
+}
+func (c *Cell) ResetWall() {
+	*c &^= wallBit
+}
+func (c *Cell) SetBox(itemType int) {
+	switch itemType {
+	case itemNone:
+		*c |= boxWithoutItemBit
+	case itemExtraRange:
+		*c |= boxWithItem1Bit
+	case itemExtraBomb:
+		*c |= boxWithItem2Bit
+	}
+}
+func (c *Cell) ResetBox() {
+	*c &^= boxBits
 }
 
-type Line [nbCols]byte
+func (c *Cell) SetItem(itemType int) {
+	switch itemType {
+	case itemExtraRange:
+		*c |= item1Bit
+	case itemExtraBomb:
+		*c |= item2Bit
+	}
+}
+func (c *Cell) ResetItem() {
+	*c &^= itemBits
+}
+
+func (c Cell) isEmpty() bool {
+	return c == 0
+}
+func (c Cell) CanReceiveBombNow() bool {
+	return c.isEmpty()
+}
+func (c Cell) isAccessible() bool {
+	return (c & inaccessibleBits) == 0
+}
+func (c Cell) isWall() bool {
+	return (c & wallBit) > 0
+}
+func (c Cell) isBox() bool {
+	return (c & boxBits) > 0
+}
+func (c Cell) isBomb() bool {
+	return (c & bombBits) > 0
+}
+func (c Cell) isPlayer() bool {
+	return (c & playerBits) > 0
+}
+func (c Cell) isItem() bool {
+	return (c & itemBits) > 0
+}
+func (c Cell) getItemType() int {
+	if c&(item1Bit|boxWithItem1Bit) > 0 {
+		return itemExtraRange
+	} else if c&(item2Bit|boxWithItem2Bit) > 0 {
+		return itemExtraBomb
+	}
+	return itemNone
+}
+func (c Cell) getPlayerIds() (nbPlayers int, playerIds [maxPlayers]int) {
+	if (c & player0Bit) > 0 {
+		playerIds[nbPlayers] = 0
+		nbPlayers++
+	}
+	if (c & player1Bit) > 0 {
+		playerIds[nbPlayers] = 1
+		nbPlayers++
+	}
+	if (c & player2Bit) > 0 {
+		playerIds[nbPlayers] = 2
+		nbPlayers++
+	}
+	if (c & player3Bit) > 0 {
+		playerIds[nbPlayers] = 3
+		nbPlayers++
+	}
+	//fmt.Fprintf(os.Stderr, "nbPlayers=%v\n", nbPlayers)
+	return
+}
+func (c Cell) getBombPlayerIds() (nbPlayers int, playerIds [maxPlayers]int) {
+	if (c & bombPlayer0Bit) > 0 {
+		playerIds[nbPlayers] = 0
+		nbPlayers++
+	}
+	if (c & bombPlayer1Bit) > 0 {
+		playerIds[nbPlayers] = 1
+		nbPlayers++
+	}
+	if (c & bombPlayer2Bit) > 0 {
+		playerIds[nbPlayers] = 2
+		nbPlayers++
+	}
+	if (c & bombPlayer3Bit) > 0 {
+		playerIds[nbPlayers] = 3
+		nbPlayers++
+	}
+	//fmt.Fprintf(os.Stderr, "nbPlayers=%v\n", nbPlayers)
+	return
+}
+
+type Line [nbCols]Cell
 type Grid [nbRows]Line
 
 func (g *Grid) acquire() {
@@ -84,23 +276,59 @@ func (g *Grid) acquire() {
 		var s string
 		fmt.Scan(&s)
 		for j := 0; j < nbCols; j++ {
-			g[i][j] = s[j]
+			switch s[j] {
+			case box:
+				g[i][j].SetBox(itemNone)
+			case boxWithExtraRange:
+				g[i][j].SetBox(itemExtraRange)
+			case boxWithExtraBomb:
+				g[i][j].SetBox(itemExtraBomb)
+			case wall:
+				g[i][j].SetWall()
+			}
 		}
 	}
 }
 
-func isBox(b byte) bool {
-	return b >= box && b <= boxWithExtraBomb
-}
-func isEmpty(b byte) bool {
-	return b == empty
-}
-func isWall(b byte) bool {
-	return b == wall
+func (cell Cell) String() string {
+	//fmt.Fprintf(os.Stderr, "raw: %o\n", int16(cell))
+
+	if cell.isEmpty() {
+		return " "
+	} else if cell.isWall() {
+		return "."
+	} else if cell.isBox() {
+		switch cell.getItemType() {
+		case itemNone:
+			return "N"
+		case itemExtraRange:
+			return "R"
+		case itemExtraBomb:
+			return "M"
+		}
+	} else if cell.isItem() {
+		switch cell.getItemType() {
+		case itemExtraRange:
+			return "r"
+		case itemExtraBomb:
+			return "m"
+		}
+	} else if cell.isBomb() {
+		_, ids := cell.getBombPlayerIds()
+		return string('a' + byte(ids[0]))
+	} else if cell.isPlayer() {
+		_, ids := cell.getPlayerIds()
+		return string('A' + byte(ids[0]))
+	}
+	return "?"
 }
 
 func (l Line) String() string {
-	return string(l[:nbCols])
+	var buffer bytes.Buffer
+	for _, cell := range l {
+		buffer.WriteString(fmt.Sprintf("%v", cell))
+	}
+	return buffer.String()
 }
 func (g Grid) String() string {
 	var buffer bytes.Buffer
@@ -110,18 +338,17 @@ func (g Grid) String() string {
 	return buffer.String()
 }
 
-func (g Grid) CellAt(p Position) byte {
-	return g[p.y][p.x]
-}
-func (g *Grid) SetCellAt(p Position, b byte) {
-	g[p.y][p.x] = b
+func (g *Grid) CellAt(p Position) *Cell {
+	return &(g[p.y][p.x])
 }
 
 type GameArea struct {
-	grid    Grid
-	bombs   []Bomb
-	players [2]Player
-	items   []Item
+	grid Grid
+
+	droppedBombs []Bomb
+	players      [maxPlayers]Player
+
+	previous *GameArea
 }
 
 func (ga *GameArea) acquire() {
@@ -130,56 +357,35 @@ func (ga *GameArea) acquire() {
 	var nbEntities int
 	fmt.Scan(&nbEntities)
 
-	ga.bombs = make([]Bomb, 0)
-	ga.items = make([]Item, 0)
+	ga.droppedBombs = make([]Bomb, 0)
 
 	for i := 0; i < nbEntities; i++ {
 		var entityType, owner, x, y, param1, param2 int
 		fmt.Scan(&entityType, &owner, &x, &y, &param1, &param2)
+		pos := Position{x, y}
 		switch entityType {
 		case playerEntity:
-			ga.players[owner] = Player{Position{x, y}, param1, param2}
+			ga.players[owner] = Player{param1, param2, false}
+			ga.grid.CellAt(pos).SetPlayer(owner)
 		case bombEntity:
-			var bomb = Bomb{Position{x, y}, owner, param1, param2}
-			ga.bombs = append(ga.bombs, bomb)
+			ga.grid.CellAt(pos).SetBomb(owner)
+			if ga.previous != nil {
+				if !ga.previous.grid.CellAt(pos).isBomb() {
+					var bomb = Bomb{pos, owner}
+					ga.droppedBombs = append(ga.droppedBombs, bomb)
+				}
+			}
 		case itemEntity:
-			ga.items = append(ga.items, Item{Position{x, y}, param1})
+			ga.grid.CellAt(pos).SetItem(param1)
 		}
 	}
 }
 
-func (ga GameArea) HasBombAt(p Position) bool {
-	for _, bomb := range ga.bombs {
-		if bomb.Position == p {
-			return true
-		}
-	}
-	return false
-}
-
-func (ga GameArea) BombCanBeDroppedAt(p Position) bool {
-	return isEmpty(ga.grid.CellAt(p)) && !ga.HasBombAt(p)
-}
-
-func (ga GameArea) CanMoveTo(p Position) bool {
-	return ga.BombCanBeDroppedAt(p)
-}
-
-func (ga *GameArea) ExplodeBomb(bombIdx int) (brokenBoxes int) {
-	bomb := ga.bombs[bombIdx]
-	for _, boxPos := range ga.GetBoxesInRangeOf(bomb.Position, bomb.ownerID) {
-		brokenBoxes++
-		ga.grid.SetCellAt(boxPos, empty)
-	}
-	ga.bombs = append(ga.bombs[:bombIdx], ga.bombs[bombIdx+1:]...)
-	return
-}
-
-func (ga GameArea) NbBoxesInRangeOf(p Position, playerIdx int) int {
+func (ga *GameArea) NbBoxesInRangeOf(p Position, playerIdx int) int {
 	return len(ga.GetBoxesInRangeOf(p, playerIdx))
 }
 
-func (ga GameArea) GetBoxesInRangeOf(p Position, playerIdx int) (boxes []Position) {
+func (ga *GameArea) GetBoxesInRangeOf(p Position, playerIdx int) (boxes []Position) {
 	boxes = make([]Position, 0)
 	for _, v := range cardinalVectors {
 		pos := p
@@ -187,12 +393,10 @@ func (ga GameArea) GetBoxesInRangeOf(p Position, playerIdx int) (boxes []Positio
 			pos = Add(pos, v)
 			if pos.IsOnGrid() {
 				cell := ga.grid.CellAt(pos)
-				if !isWall(cell) {
-					if isBox(cell) {
+				if !cell.isEmpty() {
+					if cell.isBox() {
 						boxes = append(boxes, pos)
-						break
 					}
-				} else {
 					break
 				}
 			} else {
@@ -203,72 +407,12 @@ func (ga GameArea) GetBoxesInRangeOf(p Position, playerIdx int) (boxes []Positio
 	return
 }
 
-func (ga GameArea) NbTurnsUntilNextAllowedDrop(playerIdx int) int {
-	if ga.players[playerIdx].remainingBombs != 0 {
-		return 0
-	} else {
-		minTimer := bombTimer
-		for _, bomb := range ga.bombs {
-			if bomb.ownerID == playerIdx && bomb.nbRoundsToExplosion < minTimer {
-				minTimer = bomb.nbRoundsToExplosion
-			}
-		}
-		return minTimer
-	}
-}
-
-/* For instance, if you call this function with maxDistance=8, it will return a table of 9 elements:
- * element 0: p
- * element 1: cells reachable in 1 turn
- * ...
- * element 8: cells reachable in 8 turn
- */
-func (ga GameArea) ReachableCellsFrom(p Position, maxDistance int) (cells [][]Position) {
-	cells = make([][]Position, maxDistance+1)
-	var treated Grid
-
-	cells[0] = make([]Position, 1)
-	cells[0][0] = p
-	treated.SetCellAt(p, '0')
-
-	for i := 1; i <= maxDistance; i++ {
-		if len(cells[i-1]) == 0 {
-			break //optim
-		}
-		cells[i] = make([]Position, 0)
-		for _, prev := range cells[i-1] {
-			for _, v := range cardinalVectors {
-				pos := Add(prev, v)
-				if pos.IsOnGrid() && treated.CellAt(pos) == 0 { /* was not treated before */
-					treated.SetCellAt(pos, '0'+byte(i))
-					if ga.CanMoveTo(pos) {
-						cells[i] = append(cells[i], pos)
-					} else {
-						//treated.SetCellAt(pos, ' ')
-					}
-				}
-			}
-		}
-	}
-	//fmt.Fprint(os.Stderr, treated)
-	return cells
-}
-
 func (ga GameArea) String() string {
-	var g Grid = ga.grid
-
-	for _, bomb := range ga.bombs {
-		g.SetCellAt(bomb.Position, 'x')
-	}
-
-	for i := range ga.players {
-		g.SetCellAt(ga.players[i].Position, 'A'+byte(i))
-	}
-
 	var buffer bytes.Buffer
-	for i := 0; i < nbRows; i++ {
-		buffer.WriteString(fmt.Sprintf("%v\n", g[i]))
-	}
+	buffer.WriteString(fmt.Sprintf("%v", ga.grid))
+	buffer.WriteString(fmt.Sprintf("%v\n", ga.droppedBombs))
+	buffer.WriteString(fmt.Sprintf("%v\n", ga.players))
+
 	return buffer.String()
 
 }
@@ -276,52 +420,55 @@ func (ga GameArea) String() string {
 var me int //index of me
 
 func main() {
+
+	var c Cell
+	i := 0
+
+	fmt.Fprintln(os.Stderr, i, c)
+	i++
+	c.SetWall()
+	fmt.Fprintln(os.Stderr, i, c)
+	i++
+	c.ResetWall()
+	fmt.Fprintln(os.Stderr, i, c)
+	i++
+	c.SetBomb(2)
+	fmt.Fprintln(os.Stderr, i, c)
+	i++
+	c.ResetBomb(2)
+
+	fmt.Fprintln(os.Stderr, i, c)
+	i++
+	c.SetBox(1)
+
+	fmt.Fprintln(os.Stderr, i, c)
+	i++
+	c.ResetBox()
+	fmt.Fprintln(os.Stderr, i, c)
+	i++
+	c.SetPlayer(1)
+	fmt.Fprintln(os.Stderr, i, c)
+	i++
+	c.ResetPlayer(1)
+	fmt.Fprintln(os.Stderr, i, c)
+	i++
+
 	var width, height int
 	fmt.Scan(&width, &height, &me)
+	var gameArea *GameArea = nil
+	var previous *GameArea = nil
 
 	for {
 
-		var gameArea GameArea
+		gameArea = new(GameArea)
 
 		gameArea.acquire()
+		gameArea.previous = previous
 
 		fmt.Fprint(os.Stderr, gameArea)
 
-		for len(gameArea.bombs) > 0 {
-			gameArea.ExplodeBomb(0)
-		}
-
-		reachable := gameArea.ReachableCellsFrom(gameArea.players[me].Position, nbRows+nbCols)
-		minNbTurn := gameArea.NbTurnsUntilNextAllowedDrop(me)
-
-		var bestDropPos Position
-		var bestScore int
-
-		var traceNbBoxes Grid
-		for i, row := range traceNbBoxes {
-			for j := range row {
-				traceNbBoxes[i][j] = ' '
-			}
-		}
-
-		for i := range reachable {
-			for _, pos := range reachable[i] {
-				if gameArea.BombCanBeDroppedAt(pos) {
-					cost := Max(minNbTurn, i) + 1
-					nbBoxes := gameArea.NbBoxesInRangeOf(pos, me)
-					score := nbBoxes * 1000 / cost
-					if score > bestScore {
-						bestScore = score
-						bestDropPos = pos
-					}
-					traceNbBoxes.SetCellAt(pos, '0'+byte(nbBoxes))
-				}
-			}
-		}
-
-		fmt.Fprint(os.Stderr, traceNbBoxes)
-
 		// fmt.Fprintln(os.Stderr, "Debug messages...")
-		fmt.Printf("BOMB %v %v\n", bestDropPos.x, bestDropPos.y) // Write action to stdout
+		fmt.Printf("BOMB %v %v\n", 5, 6) // Write action to stdout
+		previous = gameArea
 	}
 }
